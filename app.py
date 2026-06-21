34a727909994b23ee930b734a6ef46bd8482a8d3
luisviscarrahellie <luisviscarrahellie@gmail.com>
2026-06-20 22:24:36 -0500

    Entrega evaluacion Flask XY

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 import socket
+
 app = Flask(__name__)
-HOST = "172.20.10.2"	## Ip del servidor
-PORT = 12345			## Puerto del servidor
+HOST = "172.20.10.2"
+PORT = 12345
 
 def leer_sensor():
     try:
@@ -19,14 +20,27 @@ def leer_sensor():
         print("ERROR:", e)
         return 0, 0
 
+
 @app.route('/')
 def index():
-    ejeY, ejeX = leer_sensor() #órden ejes.
+    ejeY, ejeX = leer_sensor()
     return render_template(
         'index.html',
         ejeX=ejeX,
         ejeY=ejeY
     )
+
+
+@app.route('/flechas')
+def flechas():
+    ejeX, ejeY = leer_sensor()
+    return render_template('flechas.html', ejeX=ejeX, ejeY=ejeY)
+    
+@app.route('/leds')
+def leds():
+    ejeX, ejeY = leer_sensor()
+    return render_template('leds.html', ejeX=ejeX, ejeY=ejeY)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
- 
